1f18b079aa93e605aaf6c1d261442ce6eda3662d
aa-nikitin <a.nikitin91@mail.ru>
2020-11-26 17:43:35 +0300

    das

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
   try {
     const bbbb = await getList();
 
-    res.send('asdas - ' + bbbb);
+    res.send(bbbb);
   } catch (e) {
     res.status(400).json({ error: e });
   }
